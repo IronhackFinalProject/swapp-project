e53104ef3a771c10005f8a2991a48b4ea8df2b72
Fer Ortega <fernando.ortega79@gmail.com>
2021-12-16 01:55:36 +0100

    Chat Activated

--- client/src/pages/ChatProfile.jsx
+++ client/src/pages/ChatProfile.jsx
@@ -1,11 +1,24 @@
-import "../App.css";
+// import "../App.css";
 
-function chatprofile() {
-  return (
-    <div className="App">
-      <h2>CHAT PROFILE</h2>
-    </div>
-  );
+// function chatprofile() {
+//   return (
+//     <div className="App">
+//       <h2>CHAT PROFILE</h2>
+//     </div>
+//   );
+// }
+
+// export default chatprofile;
+import { ChatEngine } from 'react-chat-engine';
+
+function Chat() {
+	return (
+		<ChatEngine
+			projectID='8c253c6f-864b-4700-a8b0-f3789313f737'
+			userName='FerAdmin'
+			userSecret='987654321'
+		/>
+	);
 }
 
-export default chatprofile;
+export default Chat;
