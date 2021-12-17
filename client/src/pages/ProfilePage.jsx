ac170424f6ac514b0651cb704f514573435aff3c
Fer Ortega <fernando.ortega79@gmail.com>
2021-12-17 01:09:42 +0100

    Actionbar responsive

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,7 +1,7 @@
 // import Product from "../../../server/models/Product.model";
 import "../App.css";
 //import Uploadimage from "../components/Uploadimage/Uploadimage";
-
+import Actionbar from "./../components/Actionbar/Actionbar"
 
 function ProfilePage(props) {
 //  console.log(props.products)
@@ -16,6 +16,7 @@ function ProfilePage(props) {
     {/* {console.log(props.user)} */}
       <h1>Welcome {props.user.username}</h1>
       <h2>Tu id de usuario es {props.user._id} y fue creado en la fecha {props.user.createdAt}. Tus lista de productos favoritos es: {props.user.favoritos}. En la app hay un total de {props.products.length} productos</h2>
+      <Actionbar />
     </div>
   );
 }
